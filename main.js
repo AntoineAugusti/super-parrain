55e33a01c8d06ccd8788e206997966ca090df9c1
Antoine Augusti <antoine.augusti@gmail.com>
2019-08-25 22:24:52 +0200

    More navigation

--- main.js
+++ main.js
@@ -2,20 +2,23 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
-
   const page = await browser.newPage();
 
   await page.goto('https://www.super-parrain.com/login');
 
   await page.type('#username', 'antoine.augusti@gmail.com');
   await page.type('#password', process.env.PASSWORD);
-
   await page.keyboard.press('Enter');
 
-
   await page.waitForNavigation();
-
   console.log('New Page URL:', page.url());
   console.log(await page.content());
+  await page.waitFor(10000);
+
+  await page.goto("https://www.super-parrain.com/offres/habitation/parrainage-planete-oui");
+  await page.waitForNavigation();
+  console.log('New Page URL:', page.url());
+  await page.waitFor(10000);
+
   await browser.close();
 })();
